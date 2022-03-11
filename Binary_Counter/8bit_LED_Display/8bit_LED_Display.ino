46ad1dc60acbb3e230b87d4929be06ca33958b3f
Nch-MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-03-11 11:40:35 +0900

    Create 8bit_LED_Display.ino

---
+++ Binary_Counter/8bit_LED_Display/8bit_LED_Display.ino
@@ -0,0 +1,46 @@
+#define LED_Pattern_0_size  2
+#define LED_Pattern_1_size  2
+#define LED_Pattern_2_size 14
+#define LED_Pattern_3_size  2
+
+uint8_t LED_Pattern_0[LED_Pattern_0_size] = {15, 240};  //B00001111, B11110000:
+uint8_t LED_Pattern_1[LED_Pattern_1_size] = {0, 255};   //B00000000, B11111111:
+uint8_t LED_Pattern_2[LED_Pattern_2_size] = {1, 2, 4, 8, 16, 32, 64, 128, 64, 32, 16, 8, 4, 2};  //B00000001, B00000010, B00000100, B00001000, B00010000, B00100000, B01000000, B10000000:
+uint8_t LED_Pattern_3[LED_Pattern_3_size] = {85, 170};  //B01010101, B10101010:
+
+void setup() {
+  DDRD = B11111111; //D0 - D7
+}
+
+void loop() {
+  for(uint8_t uint8_t i = 0; i < 4; i++){
+    PORTD = LED_Pattern_0[0];
+    delay(500);
+    PORTD = LED_Pattern_0[1];
+    delay(500);
+  }
+  delay(1000);
+  for(uint8_t i = 0; i < 4; i++){
+    PORTD = LED_Pattern_1[0];
+    delay(500);
+    PORTD = LED_Pattern_1[1];
+    delay(500);
+  }
+  delay(1000);
+  for(uint8_t i = 0; i < 8; i++){
+    for(uint8_t o = 0; o < LED_Pattern_2_size; o++){
+      LED_Pattern_2[o];
+      delay(200);
+    }
+  }
+  delay(1000);
+  
+  delay(1000);
+  for(uint8_t i = 0; i < 4; i++){
+    PORTD = LED_Pattern_4[0];
+    delay(500);
+    PORTD = LED_Pattern_4[1];
+    delay(500);
+  }
+  delay(1000);
+}

3aef9995f85eebbd2b418c2f7e53ff1afe57e47c
Nch-MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-03-18 06:10:52 +0900

    現状動きません

--- TM1638_Module/main/main.ino
+++ TM1638_Module/main/main.ino
@@ -12,8 +12,8 @@
 #define TM1638_DIO 7
 
 //送信するコマンドのビットデータを指定, 未使用領域はゼロフィル:
-#define Write_Display_reg 0b01000000
-#define Read_Key_reg      0b01000010
+#define Write_Display_reg 0b01000100
+#define Read_Key_reg      0b01000110
 #define Set_Reg_addr      0b11000000  //下位4bitはレジスタアドレスを指定する領域です。レジスタアドレスを加算するか, レジスタアドレスとのORを取って関数に渡します:
 #define Display_ON        0b10001000  //下位3bitは輝度を指定する領域です。0b000~0b111の範囲で指定ができます。使い方は上記の通りです:
 #define Display_OFF       0b10000000
@@ -36,9 +36,19 @@ void setup() {
   pinMode(TM1638_CLK, OUTPUT);
   pinMode(TM1638_DIO, OUTPUT);
 
+  //初期化:
   digitalWrite(TM1638_STB, HIGH);
   digitalWrite(TM1638_CLK, HIGH);
   digitalWrite(TM1638_DIO,  LOW);
+
+  //デバッグ用のシリアル通信の設定:
+  Serial.begin(115200);
+
+  delay(250);
+  
+  digitalWrite(TM1638_STB,  LOW);
+  shiftOut(TM1638_DIO, TM1638_CLK, LSBFIRST, 0b10001100);
+  digitalWrite(TM1638_STB, HIGH);
 }
 
 void loop() {
@@ -83,11 +93,7 @@ void ctrlLED(uint8_t led, uint8_t stat){
 /*
  * 基板上のLEDを点灯させる時, 各LEDが持つレジスタアドレスに1を加算したデータをコマンドとして送信し, 続いて点灯状態を送信します
  */
-  if(stat == 0 || stat == 1){
-    sendData(Set_Reg_addr + led + 1, stat);
-  }else{
-    //0, 1ではなかったので無視する:
-  }
+  sendData(Set_Reg_addr + led + 1, stat & 0b00000001);
 }
 
 void ctrlLEDs(uint8_t dat){
@@ -95,12 +101,32 @@ void ctrlLEDs(uint8_t dat){
  * この関数は受け取った8bitのデータを2進数で表示する関数です。少し高度になりますが一応用意しておきます
  * 基板上に配置されている通りLED1にMSB, LED8にLSBを表示するようにしています
  */
-  sendData(Set_Reg_addr + LED1 + 1, (dat && 0b10000000) >> 7);
-  sendData(Set_Reg_addr + LED2 + 1, (dat && 0b01000000) >> 6);
-  sendData(Set_Reg_addr + LED3 + 1, (dat && 0b00100000) >> 5);
-  sendData(Set_Reg_addr + LED4 + 1, (dat && 0b00010000) >> 4);
-  sendData(Set_Reg_addr + LED5 + 1, (dat && 0b00001000) >> 3);
-  sendData(Set_Reg_addr + LED6 + 1, (dat && 0b00000100) >> 2);
-  sendData(Set_Reg_addr + LED7 + 1, (dat && 0b00000010) >> 1);
-  sendData(Set_Reg_addr + LED8 + 1, (dat && 0b00000001) >> 0);
+  sendData(Set_Reg_addr + LED1 + 1, (dat & 0b10000000) >> 7);
+  sendData(Set_Reg_addr + LED2 + 1, (dat & 0b01000000) >> 6);
+  sendData(Set_Reg_addr + LED3 + 1, (dat & 0b00100000) >> 5);
+  sendData(Set_Reg_addr + LED4 + 1, (dat & 0b00010000) >> 4);
+  sendData(Set_Reg_addr + LED5 + 1, (dat & 0b00001000) >> 3);
+  sendData(Set_Reg_addr + LED6 + 1, (dat & 0b00000100) >> 2);
+  sendData(Set_Reg_addr + LED7 + 1, (dat & 0b00000010) >> 1);
+  sendData(Set_Reg_addr + LED8 + 1, (dat & 0b00000001) >> 0);
+
+  Serial.print(" dat                    = ");
+  Serial.println(dat, BIN);
+  Serial.print("(dat & 0b10000000) >> 7 = ");
+  Serial.println((dat & 0b10000000) >> 7);
+  Serial.print("(dat & 0b01000000) >> 6 = ");
+  Serial.println((dat & 0b01000000) >> 6);
+  Serial.print("(dat & 0b00100000) >> 5 = ");
+  Serial.println((dat & 0b00100000) >> 5);
+  Serial.print("(dat & 0b00010000) >> 4 = ");
+  Serial.println((dat & 0b00010000) >> 4);
+  Serial.print("(dat & 0b00001000) >> 3 = ");
+  Serial.println((dat & 0b00001000) >> 3);
+  Serial.print("(dat & 0b00000100) >> 2 = ");
+  Serial.println((dat & 0b00000100) >> 2);
+  Serial.print("(dat & 0b00000010) >> 1 = ");
+  Serial.println((dat & 0b00000010) >> 1);
+  Serial.print("(dat & 0b00000001) >> 0 = ");
+  Serial.println((dat & 0b00000001) >> 0);
+  Serial.println();
 }

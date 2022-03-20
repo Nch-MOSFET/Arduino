e2bb7fafd49aedea78a656f3566c415ce78be5d5
Nch-MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-03-20 17:48:30 +0900

    Update 8bit.ino

--- Binary_Counter/8bit/8bit.ino
+++ Binary_Counter/8bit/8bit.ino
@@ -1,16 +1,5 @@
 #include <MsTimer2.h>
 uint8_t number = 0;
-
-void counter() {
-  PORTD = number;
-  number++;
-}
-
-void setup() {
-  DDRD = B11111111;  //D0~7:
-  MsTimer2::set(1000, counter);
-  MsTimer2::start();
-}
-
-void loop() {
-}
+void counter() {PORTD = number;　number++;}
+void setup() {DDRD = B11111111;　MsTimer2::set(1000, counter);　MsTimer2::start();}
+void loop() {}

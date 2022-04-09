7ab89957a369e955a2def098c7011d4c12ac53d7
Nch MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-04-09 16:33:59 +0900

    Update parroting.ino

--- Serial/parroting/parroting.ino
+++ Serial/parroting/parroting.ino
@@ -1,6 +1,10 @@
 uint8_t incomingByte = 0;  // 受信データ用
-char forEncorder_Large[27] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-char forEncorder_Small[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char Large[27] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+char Small[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char Mark1[16] = {' ', '!', '"', '#', '$', '%', '&', ' ', '(', ')', '*', '+', ',', '-', '.', '/'};
+char Mark2[ 7] = {':', ';', '<', '=', '>', '?', '`'};
+char Mark3[ 6] = {'[', ' ', ']', '^', '_', '`'};
+char Mark4[ 4] = {'{', ' ' , '}', '~'};
 
 void setup() {
   Serial.begin(115200); // 9600bpsでシリアルポートを開く
@@ -11,11 +15,21 @@ void loop() {
     incomingByte = Serial.read(); // 受信データを読み込む
 
     Serial.print("I received: "); // 受信データを送りかえす
-    Serial.println(incomingByte, HEX);
+    Serial.println(incomingByte, DEC);
     Serial.print("Encored   :  ");
     if(incomingByte >= 0x30 && 0x39 >= incomingByte){
       Serial.println(encorder_Num(incomingByte));
-    }else if((incomingByte >= 0x41 && 0x5A >= incomingByte) || (incomingByte >= 0x61 && 0x7A >= incomingByte)){
+    }else if(incomingByte >= 0x41 && 0x5A >= incomingByte){
+      Serial.println(encorder_Num(incomingByte));
+    }else if(incomingByte >= 0x61 && 0x7A >= incomingByte){
+      Serial.println(encorder_Chr(incomingByte));
+    }else if(incomingByte >= 0x20 && 0x2F >= incomingByte){
+      Serial.println(encorder_Chr(incomingByte));
+    }else if(incomingByte >= 0x3A && 0x40 >= incomingByte){
+      Serial.println(encorder_Chr(incomingByte));
+    }else if(incomingByte >= 0x5B && 0x60 >= incomingByte){
+      Serial.println(encorder_Chr(incomingByte));
+    }else if(incomingByte >= 0x7B && 0x7E >= incomingByte){
       Serial.println(encorder_Chr(incomingByte));
     }else{
       Serial.println();
@@ -31,11 +45,24 @@ uint8_t encorder_Num(uint8_t dat){
 char encorder_Chr(uint8_t dat){
   //A~Zの処理:
   if(dat >= 0x41 && 0x5A >= dat){
-    return forEncorder_Large[dat - 0x41];
+    return Large[dat - 0x41];
   }
   //a~zの処理:
   else if(dat >= 0x61 && 0x7A >= dat){
-    return forEncorder_Small[dat - 0x61];
+    return Small[dat - 0x61];
+  }
+  //記号とか:
+  else if(dat >= 0x20 && 0x2F >= dat){
+    return Mark1[dat - 0x20];
+  }
+  else if(dat >= 0x3A && 0x40 >= dat){
+    return Mark2[dat - 0x3A];
+  }
+  else if(dat >= 0x5B && 0x60 >= dat){
+    return Mark3[dat - 0x5B];
+  }
+  else if(dat >= 0x7B && 0x7E >= dat){
+    return Mark4[dat - 0x7B];
   }
   //例外処理:
   else{

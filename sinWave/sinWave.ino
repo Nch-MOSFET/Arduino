e229dff92ede0d763eb0fb2420357b090f919f4c
Nch MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-05-09 22:37:31 +0900

    Update sinWave.ino

--- sinWave/sinWave.ino
+++ sinWave/sinWave.ino
@@ -1,9 +1,9 @@
 /*
- * PORTDの出力にR-2R DACをつなぐと1Hzの正弦波を出力します
+ * PORTDの出力にR-2R DACをつなぐと任意周波数の正弦波を出力します
  * いい感じにdelayを調整することで周波数を変えることができます
  */
 
-#define Frequency     1
+#define delayTime     1
 #define DDR_ALLOUTPUT 0b11111111
 
 uint8_t sinWave[360];
@@ -15,9 +15,7 @@ void setup() {
     Serial.println("sinWave[" + String(i) + "] = " + String(sinWave[i]));
   }
 
-  uint16_t delayTime = 1000000 / (360 * Frequency);
-
-  DDRD = DDR_ALLOUTPUT;
+ DDRD = DDR_ALLOUTPUT;
 }
 
 void loop() {

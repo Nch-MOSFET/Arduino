5c269326ed7d3712907f3f734757192bea9d8bb7
Nch MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-05-14 22:51:16 +0900

    Update and rename TM1638_Module/TM1638_Test/main/TM1638_Driver.ino to TM1638_Module/main/TM1638_Driver.ino

--- TM1638_Module/main/TM1638_Driver.ino
+++ TM1638_Module/main/TM1638_Driver.ino
@@ -32,3 +32,25 @@ void sendDat(uint8_t dat) {
 void dataOut(uint8_t dat) {
   shiftOut(TM1638_DIO, TM1638_CLK, LSBFIRST, dat);
 }
+
+void Display_TM1638(uint8_t D1, uint8_t D2, uint8_t D3, uint8_t D4, uint8_t D5, uint8_t D6, uint8_t D7, uint8_t D8, uint8_t LED) {
+  digitalWrite(TM1638_STB,  LOW);
+  dataOut(Set_Register_Addr_00H);
+  dataOut(segments[D1]);
+  dataOut((LED && 0b10000000) >> 7);
+  dataOut(segments[D2]);
+  dataOut((LED && 0b01000000) >> 6);
+  dataOut(segments[D3]);
+  dataOut((LED && 0b00100000) >> 5);
+  dataOut(segments[D4]);
+  dataOut((LED && 0b00010000) >> 4);
+  dataOut(segments[D5]);
+  dataOut((LED && 0b00001000) >> 3);
+  dataOut(segments[D6]);
+  dataOut((LED && 0b00000100) >> 2);
+  dataOut(segments[D7]);
+  dataOut((LED && 0b00000010) >> 1);
+  dataOut(segments[D8]);
+  dataOut((LED && 0b00000001));
+  digitalWrite(TM1638_STB, HIGH);
+}

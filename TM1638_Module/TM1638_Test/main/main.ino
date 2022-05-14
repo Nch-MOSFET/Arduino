91c327dd9d3ed15429f97e4812bf27adc258dfab
Nch MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-05-14 21:49:42 +0900

    Update main.ino

--- TM1638_Module/TM1638_Test/main/main.ino
+++ TM1638_Module/TM1638_Test/main/main.ino
@@ -29,10 +29,52 @@ void setup() {
 }
 
 void loop() {
-  Display(random(1000000));
+  digitalWrite(TM1638_STB,  LOW);
+  dataOut(Set_Register_Addr_00H);
+  dataOut(segments[0]);
+  dataOut(0b00000001);
+  dataOut(segments[1]);
+  dataOut(0b00000001);
+  dataOut(segments[2]);
+  dataOut(0b00000001);
+  dataOut(segments[3]);
+  dataOut(0b00000001);
+  dataOut(segments[4]);
+  dataOut(0b00000001);
+  dataOut(segments[5]);
+  dataOut(0b00000001);
+  dataOut(segments[6]);
+  dataOut(0b00000001);
+  dataOut(segments[7]);
+  dataOut(0b00000001);
+  digitalWrite(TM1638_STB, HIGH);
+  
+  delay(1000);
+
+  digitalWrite(TM1638_STB,  LOW);
+  dataOut(Set_Register_Addr_00H);
+  dataOut(segments[8]);
+  dataOut(0b00000000);
+  dataOut(segments[9]);
+  dataOut(0b00000000);
+  dataOut(segments[10]);
+  dataOut(0b00000000);
+  dataOut(segments[11]);
+  dataOut(0b00000000);
+  dataOut(segments[12]);
+  dataOut(0b00000000);
+  dataOut(segments[13]);
+  dataOut(0b00000000);
+  dataOut(segments[14]);
+  dataOut(0b00000000);
+  dataOut(segments[15]);
+  dataOut(0b00000000);
+  digitalWrite(TM1638_STB, HIGH);
+  
   delay(1000);
 }
 
+/* 以下テスト中のコード
 void Display(uint16_t dat) {
   String txt = String(dat);
   Serial.println("dat         = " + String(dat));
@@ -40,3 +82,4 @@ void Display(uint16_t dat) {
   Serial.println("sizeof(txt) = " + String(sizeof(txt)));
   Serial.println();
 }
+ */

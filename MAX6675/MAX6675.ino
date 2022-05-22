c134491e9b1f4bbc2bf5c902edb61b706348ce61
Nch MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-05-22 11:21:40 +0900

    Update and rename main.ino to MAX6675.ino

--- MAX6675/MAX6675.ino
+++ MAX6675/MAX6675.ino
@@ -1,6 +1,5 @@
 #include "max6675.h"
 
-
 int thermoDO = 4;
 int thermoCS = 5;
 int thermoCLK = 6;
@@ -10,12 +9,10 @@ MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 
 void setup() {
   Serial.begin(115200);
-
   delay(500);
 }
 
 void loop() {
    Serial.println(thermocouple.readCelsius());
-
    delay(1000);
 }
